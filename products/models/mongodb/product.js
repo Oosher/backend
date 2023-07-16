5affd2f47f13e5af20ad64ed9f5079def3d4bde4
Oosher <105375761+Oosher@users.noreply.github.com>
2023-07-16 20:24:14 +0300

    added a stock update and delete

--- products/models/mongodb/product.js
+++ products/models/mongodb/product.js
@@ -37,7 +37,11 @@ const productSchema = new mongoose.Schema({
         minlength:3,
 
     },
-
+    stock:{
+        type:Number,
+        required:true,
+        default:1,
+    },
     price:{
         type: Number,
         required:true,
--- products/routes/productService.js
+++ products/routes/productService.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const errorService = require("../../errorHandling/errorService");
 const router = express.Router();
-
 const Product = require("../models/mongodb/product")
 
 router.get("/", async (req,res)=>{
@@ -23,10 +22,22 @@ try{
 
 
 
+router.get("/:id",async(req,res)=>{
+    try{
+        const product = await Product.findById(req.params.id);
+        res.send(product);
+    }
+    catch(err){
+        errorService("cannot find product",res)
+    }
+
+
+})
+
+
 
 
 router.post("/createnewproduct",async (req,res)=>{
-
 
     try{
     const product = new Product(req.body);
@@ -42,12 +53,47 @@ router.post("/createnewproduct",async (req,res)=>{
     }
 
 
+})
 
+
+//updates the stock
+router.put("/:id/:amount",async (req,res)=>{
+    try{
+        const product = await Product.findByIdAndUpdate(req.params.id,{$inc:{stock:req.params.amount}},{new:true});
+            res.send(product)
+
+    }catch(err){
+
+        errorService(err.message,res);
+
+    }
+    
 
 })
 
 
+router.delete("/:id",async(req,res)=>{
+    
+    try{
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+        if (deletedProduct===null) {
+            errorService("Product was not found",res);
+        }
+        else{
+        
+            res.send("Product was deleted successfully")
+        }
 
 
+    }catch(err){
+        errorService("Product was not found",res);
+
+    }
+
+
+
+
+
+})
 
 module.exports= router;
