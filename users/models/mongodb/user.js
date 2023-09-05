c3737a5fe9a782901ee8d715bab62a1e0f2dcc94
Oosher <105375761+Oosher@users.noreply.github.com>
2023-09-05 23:34:28 +0300

    fixed the bug when some images wont display

--- users/models/mongodb/user.js
+++ users/models/mongodb/user.js
@@ -41,7 +41,7 @@ const userSchema = new mongoose.Schema({
             /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
         ),
         trim: true,
-        lowercase: true,
+        
 
 
     },
